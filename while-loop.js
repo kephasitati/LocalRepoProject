b1a0f8afd8ed836017b57a47de1dde46aa8713d2
kephasitati <kephasitati@gmail.com>
2025-07-12 18:44:55 +0300

    Conditional functions

--- while-loop.js
+++ while-loop.js
@@ -6,3 +6,17 @@ while(i < names.length) {
     console.log(names[i]);
     i++;
 }
+
+
+const age = 20;
+
+if(age > 20) {
+    console.log('you are over 20 years old');
+}
+
+const ninjas = ['nuri', 'vycek', 'jane'];
+
+if(ninjas.length > 4) {
+    console.log('you have many ninjas');
+}
+
